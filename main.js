cd8e51214b2ded39b2984c12dc3c16b90db1d6af
PapiKek <papikekdev@outlook.com>
2023-09-26 13:22:07 -0700

    the bot does what it's supposed to do now(?)

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
-const {bottoken} = require('./token.json')
+/*requirements*/
 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const {Client, GatewayIntentBits, Events, Message} = require('discord.js');
+
 const client = new Client({ intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,
     ],
 });
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+
+//break
+//twitter posting
+function istwitter(foo){
+    if (foo.startsWith("https://x.com/" === true)){
+        return "1";}
+    else if (foo.startsWith("https://twitter.com/") === true){
+        return "2";
+    }
+    else if (foo.startsWith("https://vxtwitter.com/") === true){
+        return "3";
+    }
+    else return "0";
+}
+
+client.on('messageCreate', (message) => {
+    //if message.author == client
+    if (message.author.bot === false)
+    {
+        switch (istwitter(message.content)){
+            case "1":
+                message.delete();
+                message.channel.send(message.content.replace("https://x.com/","https://fxtwitter.com"));
+                break;
+
+            case "2":
+                message.delete();
+                message.channel.send(message.content.replace("https://twitter.com/","https://fxtwitter.com"));
+                break;
+
+            case "3":
+                message.delete();
+                message.channel.send(message.content.replace("https://vxtwitter.com/","https://fxtwitter.com"));
+                break;
+            default:
+                return;}}})
+
+//break
+//login and logging
+
+const {bottoken} = require('./token.json')
 client.login(bottoken);
+client.once(Events.ClientReady, c => {
+    console.log(`${c.user.tag} start`);
+});
